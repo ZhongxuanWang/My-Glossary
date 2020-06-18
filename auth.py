05b741cc8bf2567d127b27c7c46202ffe3ddbf4e
Daniel <cndanielwang@gmail.com>
2020-06-18 09:19:16 +0800

    change redirect page while login

--- auth.py
+++ auth.py
@@ -61,7 +61,7 @@ def login_post():
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
-    return redirect(url_for('profile'))
+    return redirect(url_for('index'))
 
 
 @auth.route('/signup', methods=['POST'])
