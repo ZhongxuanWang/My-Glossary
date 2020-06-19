925d3b86df51cb0494ddff11bd0691eb418206a7
Daniel <cndanielwang@gmail.com>
2020-06-19 17:03:30 +0800

    update scrape_word.py

--- scrape_word.py
+++ scrape_word.py
@@ -4,13 +4,22 @@ from bs4 import BeautifulSoup as Bs
 
 class ScrapeWord:
     json = {
+        # This is the state for whether the program grabbed the word
+        'state': False,
         'word': '',
         'dic.com': {
-
+            'state': False,
+            '': ''
         },
         'voc.com': {
-
+            'state': False,
+            'long': '',
+            'short': '',
+            'definition': ''
         },
+        # 'cam.com': {
+        #
+        # },
     }
 
     def __init__(self, word):
@@ -19,10 +28,30 @@ class ScrapeWord:
     def scrape(self):
         # Dictionary.com scrape
         soup = Bs(urllib.request.urlopen('https://www.dictionary.com/browse/' + self.json['word']), 'html.parser')
-        for num, cls in enumerate(['css-1o58fj8 e1hk9ate4', ]):
+        for cls in ['css-1o58fj8 e1hk9ate4', ]:
             try:
                 c1 = soup.find(class_=cls).get_text()
             except AttributeError:
                 break
             # self.json['']
+        # Vocabulary.com scrape
+        soup = Bs(urllib.request.urlopen('https://www.vocabulary.com/dictionary/' + self.json['word']), 'html.parser')
+        for cls in ['long', 'short', 'definitions']:
+            try:
+                c1 = soup.find(class_=cls).get_text()
+            except AttributeError:
+                break
+            # self.json['']
+
+        # # Cambridge.com support is currently under development since its classes are much messier...
+        # # Cambridge.com scrape
+        # soup = Bs(urllib.request.urlopen('https://dictionary.cambridge.org/dictionary/english/' + self.json['word']),
+        #           'html.parser')
+        # for cls in ['css-1o58fj8 e1hk9ate4', ]:
+        #     try:
+        #         c1 = soup.find(class_=cls).get_text()
+        #     except AttributeError:
+        #         break
+        #     # self.json['']
+
         return self.json
